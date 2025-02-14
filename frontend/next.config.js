d3013d4f7c1064f8fd8887eedce3625c97e9b1b7
billmark0093 <billmark0093@gmail.com>
2025-02-14 04:10:10 -0500

    initial version 1.0 uploaded

---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const old_icon = require('./public/bnb-fr.ico')
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
